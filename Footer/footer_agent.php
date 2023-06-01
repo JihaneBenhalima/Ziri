82713a6ae62e51f9134075b4263a2c30e50fce55
JihaneBenhalima <135166139+JihaneBenhalima@users.noreply.github.com>
2023-06-01 03:23:33 +0200

    Footer

---
+++ Footer/footer_agent.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="../CssFiles/footer.css">
+     <link rel="stylesheet" href="../css/all.min.css">
+ </head>
+ <body>
+     <footer id="footer">   
+         <div class="footer-logo-container">
+             <hr class="footer-line-left">
+             <hr class="footer-line-right">
+             <img src="../img/Logo/ZiriLogoBlack.png" class="footer-logo" alt="Ziri Logo">
+         </div>
+         <div class="footer-social-media">
+             <h3 class="underline" id="footer-heading">Explore Ziri</h3>
+             <div class="social-media-icons">
+                 <a href="https://fr-fr.facebook.com/" style="color: blue;" ><i class="fa-brands fa-facebook"></i></a>
+                 <a href="https://fr.linkedin.com/" style="color: #0073b1;"><i class="fa-brands fa-linkedin"></i></a>
+                 <a href="https://www.instagram.com/" style="color: #fc5c9c;"><i class="fa-brands fa-instagram"></i></a>
+             </div>
+         </div>
+         <hr>
+         <div class="footer-container">
+             <div class="footer-col">
+                 <h3 class="underline" id="footer-heading">About Us</h3>
+                 <ul class="footer-list">
+                     <li class="footer-list-item"><a href="../AgentSession/agents_agent.php">See Our agents</a></li>
+                     <li class="footer-list-item"><a href="../WhyZiri.html">Why Ziri ?</a></li>
+                     <li class="footer-list-item"><a href="../contact-us.php">Contact us</a></li>
+                 </ul>
+             </div>
+             <div class="footer-col">
+                 <h3 class="underline" id="footer-heading">Links</h3>
+                 <ul class="footer-list">
+                     <li class="footer-list-item"><a href="../AgentSession/properties_agent.php">See all properties</a></li>
+                     <li class="footer-list-item"><a href="../AgentSession/vacationRentals_agent.php">Vacation rentals</a></li>
+                     <li class="footer-list-item"><a href="#">Log in</a></li>
+                 </ul>
+             </div>
+             <div class="footer-col">
+                 <h3 class="underline" id="footer-heading">Services</h3>
+                 <ul class="footer-list">
+                     <li class="footer-list-item"><a href="../terms&conditions.html">Terms&conditions</a></li>
+                     <li class="footer-list-item"><a href="../privacypolicy.html">Privacy policy</a></li>
+                     <li class="footer-list-item"><a href="../contact-us.php">Help</a></li>
+                 </ul>
+             </div>
+         </div>
+         <div class="footer-newsletter">
+             <h3 class="underline" id="footer-heading" style="padding-top: 30px;">Subscribe to Our Newsletter</h3>
+             <form class="footer-form" method="post">
+                 <input type="email" class="footer-form-input" placeholder="Enter your email"  required>
+                 <button class="footer-form-button" name="Subscribe">Subscribe</button>
+             </form>
+         </div>
+         <div class="copy-rights">
+             <p>Copyright © [2023] [Ziri]. All rights reserved.</p>
+         </div>
+     </footer>
+ </body>
+</html>
